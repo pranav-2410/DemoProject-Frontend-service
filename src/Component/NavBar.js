064059da3000ec343d0fd96b45e0fdb7361e3a35
Pranav P <pranav0024p@gmail.com>
2024-06-15 13:37:23 +0530

    Removed routing and made deployment ready

--- src/Component/NavBar.js
+++ src/Component/NavBar.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 export default function NavBar(props) {
   return (
     <div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme={`${props.mode==="light"?"dark":"light"}`}>
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
+          <a className="navbar-brand" href="#">
             {props.title}
-          </Link>
+          </a>
           <button
             className="navbar-toggler"
             type="button"
@@ -24,15 +24,15 @@ export default function NavBar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">
+                <a className="nav-link active" aria-current="page" href="#">
                   Home
-                </Link>
+                </a>
               </li>
-              <li className="nav-item">
+              {/* <li className="nav-item">
                 <Link className="nav-link" to="/about">
                   {props.about}
                 </Link>
-              </li>
+              </li> */}
             </ul>
             <form className="d-flex mx-3" role="search">
               <input
