9be6d79560e8798bfbf62bf4af1759b009768bd3
Pranav P <pranav0024p@gmail.com>
2024-06-15 13:27:06 +0530

    Updates

---
+++ precache-manifest.58e1934db604625d19e18791ab4bcd01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4764757da6ddc660f6c240343b0cd817",
+    "url": "/DemoProject-Frontend-service/index.html"
+  },
+  {
+    "revision": "0db0427cd2f7ca6a1a86",
+    "url": "/DemoProject-Frontend-service/static/css/main.8313b480.chunk.css"
+  },
+  {
+    "revision": "d5484beb43929c9a621e",
+    "url": "/DemoProject-Frontend-service/static/js/2.10bdf04c.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/DemoProject-Frontend-service/static/js/2.10bdf04c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a305264669ab0698eb9",
+    "url": "/DemoProject-Frontend-service/static/js/3.a8bd2e92.chunk.js"
+  },
+  {
+    "revision": "0db0427cd2f7ca6a1a86",
+    "url": "/DemoProject-Frontend-service/static/js/main.958e42e3.chunk.js"
+  },
+  {
+    "revision": "e69442c86f99dec771b6",
+    "url": "/DemoProject-Frontend-service/static/js/runtime-main.1cb59363.js"
+  }
+]);
